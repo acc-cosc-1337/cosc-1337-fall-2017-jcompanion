bfef3f6540244cb7699a6c4a441d343ec1d472d2
jcompanion <31940636+jcompanion@users.noreply.github.com>
2017-09-20 18:43:44 -0500

    added payroll prototypes to folder

--- src/Payroll.cpp
+++ src/Payroll.cpp
@@ -1,4 +1,23 @@
 #include "Payroll.h"
 #include <math.h>
 
+#include "Payroll.h"
+
+
+bool isValidSalaryAmount(int a_sal)
+
+{
+	return a_sal >= 20000 && a_sal <= 100000;
+}
+
+bool isValidHourlyAmount(int pay_rate)
+{
+	return pay_rate >= 0 && pay_rate <= 60;
+
+}
+bool isValidAmountWorked(int hours)
+{
+
+	return hours >= 0 && hours <= 50;
+}
 
