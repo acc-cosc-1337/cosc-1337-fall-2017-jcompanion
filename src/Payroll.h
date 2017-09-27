22c86f6ac75567b7a1b3c4c9d8696967eff2cdae
jcompanion <31940636+jcompanion@users.noreply.github.com>
2017-09-27 18:21:29 -0500

    updates boolean function names

--- src/Payroll.h
+++ src/Payroll.h
@@ -1,10 +1,16 @@
-#ifndef _PAYROLL_H
-#define _PAYROLL_H
+#ifndef PAYROLL_H_INCLUDED
+#define PAYROLL_H_INCLUDED
+#include <string>
+using namespace std;
 
+const int EMPLOYEES = 3;
+const double MAX_HOURS = 40.0;
+const double MULTIPLIER = 1.5;
+const double FIT = .15;
+const double FICA_SS = .062;
+const double FICA_MED = .0145;
 bool isValidSalaryAmount(int a_sal);
-bool isValidHourlyAmount(int hours);
-bool isValidAmountWorked(int pay_rate);
-double getGrossPay(double hours, double ot_hours, double pay_rate);
+bool isValidPayRate(int hours);
+bool isValidHoursWorked(int pay_rate);
 
-
-#endif
+#endif // PAYROLL_H_INCLUDED
