709d342493a30de91303f9c0c9dcf8f1290bba1d
jcompanion <31940636+jcompanion@users.noreply.github.com>
2017-09-27 18:23:30 -0500

    updated .cpp file to accommodate arrays

--- src/Payroll.cpp
+++ src/Payroll.cpp
@@ -1,7 +1,6 @@
 #include "Payroll.h"
-#include <math.h>
+#include "stdafx.h"
 
-#include "Payroll.h"
 
 
 bool isValidSalaryAmount(int a_sal)
@@ -10,20 +9,13 @@ bool isValidSalaryAmount(int a_sal)
 	return a_sal >= 20000 && a_sal <= 100000;
 }
 
-bool isValidHourlyAmount(int pay_rate)
+bool isValidPayRate(int pay_rate)
 {
 	return pay_rate >= 0 && pay_rate <= 60;
 
 }
-bool isValidAmountWorked(int hours)
+bool isValidHoursWorked(int hours)
 {
 
 	return hours >= 0 && hours <= 50;
-}
-
-double getGrossPay(double hours, double ot_hours, double pay_rate)
-{
-    double gross_pay = hours * pay_rate;
-
-    return gross_pay;
 }
