bf9daa13d2fc55fdd1bafc6fad188012685ad08d
jcompanion <31940636+jcompanion@users.noreply.github.com>
2017-09-27 18:48:09 -0500

    Update payroll_test.cpp

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -1,8 +1,20 @@
 #include "gtest/gtest.h"
 #include "Payroll.h"
 
-TEST(getGrosspay, payRollTest){
-    EXPECT_EQ(0, getGrosspay(30));
+TEST(PayRollTest, test_if_invalid_salary_when_value_is_less_than_0)
+{
+    EXPECT_EQ(false, isValidSalaryAmount(10000));
 }
 
-#EXPECT_EQ(expectedValue, actualValue);
+TEST(PayRollTest, test_if_valid_salary_when_value_is_less_than_0)
+{
+    EXPECT_EQ(true, isValidSalaryAmount(30000));
+}
+TEST(PayRollTest, test_if_valid_hours_when_value_is_more_than_0)
+{
+    EXPECT_EQ(true, isValidSalaryAmount(20));
+}
+TEST(PayRollTest, test_if_invalid_hours_when_value_is_greater_than_60)
+{
+    EXPECT_EQ(false, isValidSalaryAmount(70));
+}
