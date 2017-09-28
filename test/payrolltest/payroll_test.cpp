9a2863bd7b223e23ffabe11341c2d5261e631e23
jcompanion <31940636+jcompanion@users.noreply.github.com>
2017-09-27 19:19:56 -0500

    Update payroll_test.cpp

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -12,9 +12,17 @@ TEST(PayRollTest, test_if_valid_salary_when_value_is_less_than_0)
 }
 TEST(PayRollTest, test_if_valid_hours_when_value_is_more_than_0)
 {
-    EXPECT_EQ(true, isValidSalaryAmount(30));
+    EXPECT_EQ(true, isValidHoursWorked(30));
 }
 TEST(PayRollTest, test_if_invalid_hours_when_value_is_greater_than_60)
 {
-    EXPECT_EQ(false, isValidSalaryAmount(70));
+    EXPECT_EQ(false, isValidHoursWorked(70));
+}
+TEST(PayRollTest, test_if_invalid_wage_when_value_is_greater_than_60)
+{
+    EXPECT_EQ(false, isValidPayRate(70));
+}
+TEST(PayRollTest, test_if_valid_hours_when_value_is_greater_than_0)
+{
+    EXPECT_EQ(true, isValidPayRate(20));
 }
